56732d9e3f71862052ac41fdb3b9dc39169d4dda
Nicole van den Dries <n.j.a.vandendries@students.uu.nl>
2016-06-06 17:04:45 +0200

    een werkende site!!!!!!!!

--- application/config/database.php
+++ application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'dsn'	=> 'sqlite:./db/database2.sqlite3',
+	'hostname' => '',
 	'username' => '',
 	'password' => '',
 	'database' => '',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
@@ -93,4 +93,5 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
+
 );
